a8fd6b0caae8f45d25d3dd6a340aba1e56030211
penumbra23 <glbranimir@gmail.com>
2019-09-18 19:00:00 +0200

    Add gas station as estimator source

---
+++ EthereumBatcher.Lib/GasEstimation/GasStation/EstimationModel.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace EthereumBatcher.Lib.GasEstimation.GasStation
+{
+    /// <summary>
+    /// API model for gas station estimate endpoint.
+    /// </summary>
+    public class EstimationModel
+    {
+        [JsonProperty("fast")]
+        public double Fast { get; set; }
+
+        [JsonProperty("fastest")]
+        public double Fastest { get; set; }
+
+        [JsonProperty("safeLow")]
+        public double SafeLow { get; set; }
+
+        [JsonProperty("average")]
+        public double Average { get; set; }
+
+        [JsonProperty("block_time")]
+        public double BlockTime { get; set; }
+
+        [JsonProperty("blockNum")]
+        public long BlockNum { get; set; }
+
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+
+        [JsonProperty("safeLowWait")]
+        public double SafeLowWait { get; set; }
+
+        [JsonProperty("avgWait")]
+        public double AvgWait { get; set; }
+
+        [JsonProperty("fastWait")]
+        public double FastWait { get; set; }
+
+        [JsonProperty("fastestWait")]
+        public double FastestWait { get; set; }
+    }
+}
